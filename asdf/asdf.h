89a603edf1f4d44ac498d4b27b2fff81b1315286
Jackson Taylor <jackson.taylor@willowtreeapps.com>
2015-07-09 16:43:03 -0400

    added in the code signing internal configuration

---
+++ asdf/asdf.h
@@ -0,0 +1,19 @@
+//
+//  asdf.h
+//  asdf
+//
+//  Created by buildagent on 7/9/15.
+//  Copyright © 2015 WillowTreeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for asdf.
+FOUNDATION_EXPORT double asdfVersionNumber;
+
+//! Project version string for asdf.
+FOUNDATION_EXPORT const unsigned char asdfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <asdf/PublicHeader.h>
+
+
